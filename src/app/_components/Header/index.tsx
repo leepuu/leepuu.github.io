04f72be357fd9366aba0a75f6a1e578636846c50
HL0421 <prlee@hivelab.co.kr>
2024-05-09 12:28:15 +0900

    Update

--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -1,11 +1,11 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 export default function Header() {
-  return(
-    <header className="bg-white sticky top-0 px-25">
+  return (
+    <header className='px-25 sticky top-0 bg-white'>
       <h1>
-        <Link href="/">푸다닥 개발뽀개기</Link>
+        <Link href='/'>푸다닥 개발뽀개기</Link>
       </h1>
     </header>
-  )
+  );
 }
